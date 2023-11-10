e442a45a0003a038dcdf8d00335c9ddfcd90adfd
John Jairo Rojas <79581520+JohnRojas06289@users.noreply.github.com>
2023-11-09 19:22:24 -0500

    Finalización estructura testimonio y carrusel

--- js/script.js
+++ js/script.js
@@ -1,52 +1,3 @@
-/* Carrusel*/
-const body = document.body
-const slides = document.querySelectorAll('.carrousel')
-const leftBtn = document.getElementById('left')
-const rightBtn = document.getElementById('right')
-
-let activeSlide = 0
-
-rightBtn.addEventListener('click', () => {
-  activeSlide++
-
-  if (activeSlide > slides.length - 1) {
-    activeSlide = 0
-  }
-
-  setBgToBody()
-  setActiveSlide()
-})
-
-leftBtn.addEventListener('click', () => {
-  activeSlide--
-
-  if (activeSlide < 0) {
-    activeSlide = slides.length - 1
-  }
-
-  setBgToBody()
-  setActiveSlide()
-})
-
-setBgToBody()
-
-function setBgToBody() {
-  body.style.backgroundImage = slides[activeSlide].style.backgroundImage
-}
-
-function setActiveSlide() {
-  slides.forEach((slide) => slide.classList.remove('active'))
-
-  slides[activeSlide].classList.add('active')
-}
-
-
-
-
-
-
-
-
 
 
 
@@ -82,7 +33,9 @@ function reloadSlider(){
     dots[active].classList.add('active');
 
     clearInterval(refreshInterval);
-    refreshInterval = setInterval(()=> {next.click()}, 3000); 
+    refreshInterval = setInterval(()=> {next.click()}, 3000);
+
+    
 }
 
 dots.forEach((li, key) => {
@@ -94,6 +47,7 @@ dots.forEach((li, key) => {
 window.onresize = function(event) {
     reloadSlider();
 };
+
 /*Carrusel end*/
 
 
@@ -110,96 +64,3 @@ window.onresize = function(event) {
 
 
 
-
-
-
-
-
-$(document).ready(function() {
-  function cloneSlides() {
-    const $slider = $('.slider');
-    const $slideTrack1 = $slider.find('.slide-track1');
-    const $slideTrack2 = $slider.find('.slide-track2');
-    $slideTrack1.append($slideTrack1.html());
-    $slideTrack2.append($slideTrack2.html());
-  }
-
-  cloneSlides();
-
-  // Espera a que la clonación se complete antes de iniciar la animación
-  setTimeout(function() {
-    const $slider = $('.slider');
-    const $slideTrack1 = $slider.find('.slide-track1');
-    const $slideTrack2 = $slider.find('.slide-track2');
-    $slideTrack1.css('animation', 'scroll-up 10s linear infinite');
-    $slideTrack2.css('animation', 'scroll-down 10s linear infinite');
-  }, 0);
-});
-
-
-
-
-
-// testimonial section
-const testimonialsContainer = document.querySelector('.testimonials-container')
-const testimonial = document.querySelector('.testimonial')
-const userImage = document.querySelector('.user-image')
-const username = document.querySelector('.username')
-const role = document.querySelector('.role')
-
-const testimonials = [
-  {
-    name: 'Elizabeth Gutierrez',
-    position: 'Lawyer - Prochampions S.A.S.',
-    photo:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6',
-    text:
-      "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code.",
-  },
-  {
-    name: 'Diego Romero',
-    position: 'Blockchain Develpoer',
-    photo: 'https://images.unsplash.com/photo-1606663889134-b1dedb5ed8b7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=387&q=80',
-    text:
-      'I love this guy, is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!',
-  },
-  {
-    name: 'Andres aldana - Prochampions S.A.S.',
-    position: 'Retail Director',
-    photo: 'https://randomuser.me/api/portraits/women/68.jpg',
-    text:
-      "This guy is a hard worker. Communication was also very good with him and he was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
-  },
-  {
-    name: 'Camilo Beltran',
-    position: 'Graphic Designer - Prochampions S.A.S.',
-    photo: 'https://randomuser.me/api/portraits/men/43.jpg',
-    text:
-      "I had my concerns that due to a tight deadline this project can't be done. But this guy proved me wrong not only he delivered an outstanding work but he managed to deliver 1 day prior to the deadline. And when I asked for some revisions he made them in MINUTES. I'm looking forward to work with him again and I totally recommend him. Thanks again!",
-  },
-  {
-    name: 'Jose Franco',
-    position: 'Principal Director - Franservis',
-    photo: 'https://randomuser.me/api/portraits/men/97.jpg',
-    text:
-      'This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.',
-  },
-]
-
-let idx = 1
-
-function updateTestimonial() {
-  const { name, position, photo, text } = testimonials[idx]
-
-  testimonial.innerHTML = text
-  userImage.src = photo
-  username.innerHTML = name
-  role.innerHTML = position
-
-  idx++
-
-  if (idx > testimonials.length - 1) {
-    idx = 0
-  }
-}
-setInterval(updateTestimonial, 10000);
